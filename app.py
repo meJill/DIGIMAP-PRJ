fe53d1bf257b18fd8f8875f321de4c9d6cdbea4d
Jaime Gabriel G. Magsino <59905030+JaimeMagsino@users.noreply.github.com>
2024-04-10 15:59:09 +0800

    deploy real

--- app.py
+++ app.py
@@ -31,15 +31,16 @@ def upload_file():
             filename = file.filename
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
+            h_value = float(request.form['h_value'])  # Get the value of h from the form
             img = cv2.imread(filepath)
-            denoised_img = perform_nlm_denoising(img)
+            denoised_img = perform_nlm_denoising(img, h=h_value)  # Pass h_value to the function
             output_filename = 'denoised_' + filename
             output_filepath = os.path.join(app.config['OUTPUT_FOLDER'], output_filename)
             cv2.imwrite(output_filepath, denoised_img)
             return render_template('result.html', filename=output_filename)
     return redirect(url_for('index'))
 
-def perform_nlm_denoising(image):
+def perform_nlm_denoising(image, h=20):  # Add a default value for h
     # Check if the input image is not None
     if image is None:
         return None
@@ -47,7 +48,7 @@ def perform_nlm_denoising(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # H defines the filter strength (more =  blurry, less = less filter)
-    denoised_image = cv2.fastNlMeansDenoising(gray_image, None, h=20)
+    denoised_image = cv2.fastNlMeansDenoising(gray_image, None, h=h)
 
     return denoised_image
 
